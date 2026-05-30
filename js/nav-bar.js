13592818f105e5bf12e380a0d9ac3376a5400156
Stacy <stacychapa@hotmail.com>
2026-05-30 06:56:38 -0600

    Better css

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -1,22 +1,31 @@
-function loadNavbar(){
-    navbar.innerHTML = `
-    <header>
-    <div class="container">
-        <section class="nav-bar">
-                <a href="/index.html">
-                    <img class="logo" src="../images/wolfLogo.png" alt="logo">
-                </a>
-                <nav>
-                    <ul class="nav_links">
-                        <li><a href="/index.html">Game</a></li>
-                        <li><a href="/html/team.html">Team</a></li>
-                        <li><a href="/html/contact.html">Contact</a></li>
-                    </ul>
-                </nav>
-        </section>
+function loadNavbar() {
+  navbar.innerHTML = `
+  <header>
+    <div class="nav-bar">
+      <a href="/index.html">
+        <img class="logo" src="/images/wolfLogo.png" alt="logo">
+      </a>
+      <button class="nav-toggle" id="navToggle" aria-label="Toggle menu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <nav id="navMenu">
+        <ul class="nav_links">
+          <li><a href="/index.html">Game</a></li>
+          <li><a href="/html/team.html">Team</a></li>
+          <li><a href="/html/contact.html">Contact</a></li>
+        </ul>
+      </nav>
     </div>
-    </header>`;
+  </header>`;
 
+  const toggle = document.getElementById('navToggle');
+  const menu = document.getElementById('navMenu');
+
+  toggle.addEventListener('click', () => {
+    menu.classList.toggle('open');
+  });
 }
 
 loadNavbar();
